904263d6f90187b8b836c9abd8670f1e54d9f949
Ed Scheinerman <ers@jhu.edu>
2022-02-13 09:54:16 -0500

    Formatting

--- extras/parallel_score_all.jl
+++ extras/parallel_score_all.jl
@@ -9,11 +9,11 @@ repetitions and possible words).
 
 Start up with `julia -p 8` (or whatever number of processors available).
 Then `include("parallel_score_all.jl")`
-""" 
-function parallel_score_all(scorer::Function, reps::Int=1)
+"""
+function parallel_score_all(scorer::Function, reps::Int = 1)
     NA = WordleSolver.NA
     total = 0
-    for k=1:reps
+    for k = 1:reps
         println("Round $k of $reps")
         subtotal = @distributed (+) for a = 1:NA
             auto_play(a, scorer = scorer)
