697eea5520fa45d6d2e8866690fec3766fd8eca3
Ed Scheinerman <ers@jhu.edu>
2022-04-27 16:49:20 -0400

    work

--- extras/worst_word.jl
+++ extras/worst_word.jl
@@ -1,15 +1,22 @@
 using WordleSolver, ProgressMeter
 
-function compute_average_steps(scorer::Function=min_max_score, reps::Int=10)
+function worst_steps(scorer::Function=min_max_score, reps::Int=10)
     d = Dict{String,Float64}()
     PM = Progress(WordleSolver.NA)
+    worst_avg = 0.0
+    worst_word = "XXXXX"
     for w ∈ WordleSolver.ANS_LIST
         s = 0
         for _ = 1:reps
             s += auto_play(w, scorer=scorer, verbose=false)
         end
-        d[w] = s / reps
+        avg_steps = s/reps
+        if avg_steps > worst_avg
+            worst_word = w 
+            worst_avg = avg
+        end
+
         next!(PM)
     end
-    return d
+    return worst_word
 end
