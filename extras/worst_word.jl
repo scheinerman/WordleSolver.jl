84f730a29cab1538cc4b2aa1fbc9e92bc29c05d5
Ed Scheinerman <ers@jhu.edu>
2022-04-27 20:12:31 -0400

    Working

--- extras/worst_word.jl
+++ extras/worst_word.jl
@@ -1,10 +1,16 @@
 using WordleSolver, ProgressMeter
 
-function worst_steps(scorer::Function=min_max_score, reps::Int=10)
+"""
+    worst_word(scorer::Function=min_max_score, reps::Int=5)
+
+Find the word that scores the worst using scoring method `scorer`.
+"""
+function worst_word(scorer::Function=min_max_score, reps::Int=5)
     d = Dict{String,Float64}()
     PM = Progress(WordleSolver.NA)
     worst_avg = 0.0
     worst_word = "XXXXX"
+    @info "Finding most difficult wordle word using scorer $scorer"
     for w ∈ WordleSolver.ANS_LIST
         s = 0
         for _ = 1:reps
@@ -13,7 +19,7 @@ function worst_steps(scorer::Function=min_max_score, reps::Int=10)
         avg_steps = s/reps
         if avg_steps > worst_avg
             worst_word = w 
-            worst_avg = avg
+            worst_avg = avg_steps
         end
 
         next!(PM)
