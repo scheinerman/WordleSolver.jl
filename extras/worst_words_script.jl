8fefeaed7e7f5ad3b36c7acd2c901cf95876d38b
Ed Scheinerman <ers@jhu.edu>
2022-04-28 17:10:58 -0400

    Add runtime script

--- extras/worst_words_script.jl
+++ extras/worst_words_script.jl
@@ -1,4 +1,4 @@
-include("~/.julia/dev/WordleSolver/extras/worst_words.jl")
+include(homedir() * "/.julia/dev/WordleSolver/extras/worst_words.jl")
 reps = 10
 T = make_all_tables(reps)
 full_report(T)
