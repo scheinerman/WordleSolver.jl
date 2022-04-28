f170f78b5e47d26741b1e518770aa915de09ba5c
Ed Scheinerman <ers@jhu.edu>
2022-04-28 16:25:04 -0400

    Code simplified

--- extras/worst_words.jl
+++ extras/worst_words.jl
@@ -1,5 +1,9 @@
 using WordleSolver, ProgressMeter
 
+function multiplay(word::String, scorer::Function, reps::Int)
+    return [auto_play(word, scorer = scorer, verbose = false) for _ = 1:reps]
+end
+
 """
     scorer_step_table(scorer::Function=min_max_score, reps::Int=10)
 Use the `scorer` to play all possible wordle words `reps` times each
@@ -9,15 +13,19 @@ function scorer_step_table(scorer::Function = min_max_score, reps::Int = 5)
     d = Dict{String,Float64}()
     PM = Progress(WordleSolver.NA)
     for w ∈ WordleSolver.ANS_LIST
-        s = 0
-        for _ = 1:reps
-            s += auto_play(w, scorer = scorer, verbose = false)
-        end
+        s = sum(multiplay(w, scorer, reps))
         d[w] = s / reps
         next!(PM)
     end
     return d
 end
+
+
+function worst(d::Dict{T,S}) where {T,S<:Real}
+    m = maximum(values(d))
+    return [w for w in keys(d) if d[w] == m]
+end
+
 
 """
     worst_words(scorer::Function=min_max_score, reps::Int=5)
@@ -26,6 +34,5 @@ Find the words that scores the worst using scoring method `scorer`.
 """
 function worst_words(scorer::Function = min_max_score, reps::Int = 5)
     d = scorer_step_table(scorer, reps)
-    m = maximum(values(d))
-    return [w for w in WordleSolver.ANS_LIST if d[w] == m]
+    return worst(d)
 end
